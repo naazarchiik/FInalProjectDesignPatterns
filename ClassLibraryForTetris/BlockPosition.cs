ac0b0e17a5d71c37ed3757e6ece2b86cbf7daab0
Nazarii Kryvolap <ipz222_kno@student.ztu.edu.ua>
2024-05-10 17:20:12 +0300

    BlockPosition class added

---
+++ ClassLibraryForTetris/BlockPosition.cs
@@ -0,0 +1,14 @@
+﻿namespace ClassLibraryForTetris
+{
+    public class BlockPosition
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+
+        public BlockPosition(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+    }
+}
