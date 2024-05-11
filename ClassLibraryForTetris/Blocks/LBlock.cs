7bea4eb6d538859610078a153cbe6e104a1c4cb8
Nazarii Kryvolap <ipz222_kno@student.ztu.edu.ua>
2024-05-11 17:32:58 +0300

    Oblock class added

--- ClassLibraryForTetris/Blocks/LBlock.cs
+++ ClassLibraryForTetris/Blocks/LBlock.cs
@@ -3,12 +3,12 @@
     public class LBlock : Block
     {
         private readonly BlockPosition[][] tiles = new BlockPosition[][]
-       {
+        {
             new BlockPosition[] { new(0, 2), new(1, 0),  new(1, 1), new(1, 2) },
             new BlockPosition[] { new(0, 1), new(1, 1),  new(2, 1), new(2, 2) },
             new BlockPosition[] { new(1, 0), new(1, 1),  new(1, 2), new(2, 0) },
             new BlockPosition[] { new(0, 0), new(0, 1),  new(1, 1), new(2, 1) }
-       };
+        };
 
         public override int Id => 3;
         protected override BlockPosition StartOffset => new BlockPosition(0, 3);
---
+++ ClassLibraryForTetris/Blocks/OBlock.cs
@@ -0,0 +1,14 @@
+﻿namespace ClassLibraryForTetris.Blocks
+{
+    public class OBlock : Block
+    {
+        private readonly BlockPosition[][] tiles = new BlockPosition[][]
+        {
+            new BlockPosition[] { new(0, 0), new(0, 1),  new(1, 0), new(1, 1) },
+        };
+
+        public override int Id => 4;
+        protected override BlockPosition StartOffset => new BlockPosition(0, 4);
+        protected override BlockPosition[][] Tiles => tiles;
+    }
+}
