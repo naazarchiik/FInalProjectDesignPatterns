84316cb69a6cde4300320f9dacc29295bc978112
Nazarii Kryvolap <ipz222_kno@student.ztu.edu.ua>
2024-05-11 17:28:48 +0300

    Lblock class added

--- ClassLibraryForTetris/Blocks/JBlock.cs
+++ ClassLibraryForTetris/Blocks/JBlock.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ClassLibraryForTetris.Blocks
+﻿namespace ClassLibraryForTetris.Blocks
 {
-    internal class JBlock
+    public class JBlock : Block
     {
+        private readonly BlockPosition[][] tiles = new BlockPosition[][]
+        {
+            new BlockPosition[] { new(0, 0), new(1, 0),  new(1, 1), new(1, 2) },
+            new BlockPosition[] { new(0, 1), new(0, 2),  new(1, 1), new(2, 1) },
+            new BlockPosition[] { new(1, 0), new(1, 1),  new(1, 2), new(2, 2) },
+            new BlockPosition[] { new(0, 1), new(1, 1),  new(2, 0), new(2, 1) }
+        };
+
+        public override int Id => 2;
+        protected override BlockPosition StartOffset => new BlockPosition(0, 3);
+        protected override BlockPosition[][] Tiles => tiles;
     }
 }
---
+++ ClassLibraryForTetris/Blocks/LBlock.cs
@@ -0,0 +1,17 @@
+﻿namespace ClassLibraryForTetris.Blocks
+{
+    public class LBlock : Block
+    {
+        private readonly BlockPosition[][] tiles = new BlockPosition[][]
+       {
+            new BlockPosition[] { new(0, 2), new(1, 0),  new(1, 1), new(1, 2) },
+            new BlockPosition[] { new(0, 1), new(1, 1),  new(2, 1), new(2, 2) },
+            new BlockPosition[] { new(1, 0), new(1, 1),  new(1, 2), new(2, 0) },
+            new BlockPosition[] { new(0, 0), new(0, 1),  new(1, 1), new(2, 1) }
+       };
+
+        public override int Id => 3;
+        protected override BlockPosition StartOffset => new BlockPosition(0, 3);
+        protected override BlockPosition[][] Tiles => tiles;
+    }
+}
