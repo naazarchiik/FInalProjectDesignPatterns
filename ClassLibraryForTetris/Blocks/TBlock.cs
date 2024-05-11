c5113f2fa11eed8c12fe0152687b8d8c211c5674
Nazarii Kryvolap <ipz222_kno@student.ztu.edu.ua>
2024-05-11 17:39:10 +0300

    Tblock class added

---
+++ ClassLibraryForTetris/Blocks/TBlock.cs
@@ -0,0 +1,17 @@
+﻿namespace ClassLibraryForTetris.Blocks
+{
+    public class TBlock : Block
+    {
+        private readonly BlockPosition[][] tiles = new BlockPosition[][]
+        {
+            new BlockPosition[] { new(0, 1), new(1, 0),  new(1, 1), new(1, 2) },
+            new BlockPosition[] { new(0, 1), new(1, 1),  new(1, 2), new(2, 1) },
+            new BlockPosition[] { new(1, 0), new(1, 1),  new(1, 2), new(2, 1) },
+            new BlockPosition[] { new(0, 1), new(1, 0),  new(1, 1), new(2, 1) }
+        };
+
+        public override int Id => 6;
+        protected override BlockPosition StartOffset => new BlockPosition(0, 3);
+        protected override BlockPosition[][] Tiles => tiles;
+    }
+}
